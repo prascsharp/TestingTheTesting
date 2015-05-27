b8665fb2b16294e0006ae3d6a3b74c902150488d
Prasanna Krishnaswamy <pras.csharp@gmail.com>
2015-05-27 14:36:09 +0530

    For testing travis
    The code block contains a calculator class (under that an Add method).
    There is also unit test written. This is for testing how to use
    Travis-CI.

---
+++ TestingCI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingCI
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adding 1 and 2 results {0}", new Calculator().Add(1, 2));
+        }
+    }
+}
---
+++ TestingCITests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingCITests
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            int result = new TestingCI.Calculator().Add(1,2);
+            Assert.AreEqual(result, 3);
+        }
+    }
+}
